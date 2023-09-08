a0e1aef512812fc87fe5fe7e53f15370e0171809
nehalsahukar19 <96016289+nehalsahukar19@users.noreply.github.com>
2023-09-08 19:47:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 def count_batteries_by_health(present_capacities):
-    rated_capacity= 120.0
+    # Rated capacity of the new battery
+    rated_capacity= 120.0       
     soh_vals=[]
     for i in present_capacities:
         if i<0 or i>rated_capacity:
             continue
-        soh=  100 * (i/ rated_capacity)
+         # calculate SoH
+        soh=  100 * (i/ rated_capacity)    
         soh_vals.append(soh)
     soh=soh_vals
+    # Initialising the battery counts 
     health=0
     exchange=0
     fail=0
-    for i in soh:
-        if i>80.00 :
+    # Classifying all the present capacities based on SoH values
+    for i in soh:            
+        if i>80.00 :                  
             health+=1
         elif i<=80.00 and i>=65.00:
             exchange+=1
@@ -28,12 +32,13 @@ def test_bucketing_by_health():
   print("Counting batteries by SoH...\n")
   present_capacities = [115, 118, 80, 95, 91, 77,-100,125]
   counts = count_batteries_by_health(present_capacities)
+  # Assertions to check correctness
   assert(counts["healthy"] == 2)
   assert(counts["exchange"] == 3)
   assert(counts["failed"] == 1)
-#   print(counts["healthy"])
-#   print(counts["exchange"])
-#   print(counts["failed"])
+#   print(counts["healthy"])     --- healthy batteries count 
+#   print(counts["exchange"])    --- exchange batteries count 
+#   print(counts["failed"])      --- failed batteries count 
   print("Done counting :)")
 
 
